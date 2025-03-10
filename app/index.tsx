1854561fe2781532741dfa477bf5ab25ed28b4d6
HowardPeter <phuocnt1611@gmail.com>
2025-03-10 13:10:56 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function StartPage() {
+  return (
+    <View className="bg-red-200 pt-10">
+      <Text className="text-3xl text-center">Home</Text>
+    </View>
+  );
+}
